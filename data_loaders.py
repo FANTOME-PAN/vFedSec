cb00b76eb8b059c3845a9113c74aea1e1dc22cd8
FANTOME <indv.panh@gmail.com>
2023-04-21 11:54:55 +0100

    adapt to bank

--- data_loaders.py
+++ data_loaders.py
@@ -70,11 +70,9 @@ def get_sample_selector(df: pd.DataFrame) -> ISampleSelector:
     return ExampleSampleSelector(df)
 
 
-categorical_columns = {
-    'pid', 'cms_group_id', 'final_gender_code', 'age_level', 'pvalue_level',
-    'shopping_level', 'occupation', 'new_user_class_level ', 'cate_id', 'brand'
-}
-target_column = 'clk'
+categorical_columns = {'job', 'marital', 'education', 'default', 'housing',
+                       'loan', 'contact', 'day', 'month', 'poutcome', 'y'}
+target_column = 'y'
 
 
 class ExampleDataLoader(IDataLoader):
