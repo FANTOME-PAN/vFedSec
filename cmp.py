97144fb3dbca335cd0e8213f95cdd096c7d6b5d5
Heng Pan <ac.panh99@gmail.com>
2023-08-07 22:03:52 +0100

    update cmp

--- cmp.py
+++ cmp.py
@@ -5,11 +5,21 @@ import time
 
 
 class Timer:
+    mark: float
+    cnt: int = 0
+    total: float = 0.
+
     def tic(self):
         self.mark = time.time()
 
     def toc(self):
-        return time.time() - self.mark
+        dt = time.time() - self.mark
+        self.total += dt
+        self.cnt += 1
+        return dt
+
+    def avg(self):
+        return self.total / self.cnt
 
 
 timer = Timer()
@@ -17,9 +27,17 @@ timer = Timer()
 
 def paillier_encrypt_matrix(matrix, public_key):
     encrypted_matrix = []
+    encrypt_timer = Timer()
     for row in matrix:
-        encrypted_row = [public_key.encrypt(int(x)) for x in row]
+        encrypted_row = []
+        for x in row:
+
+            encrypt_timer.tic()
+            res = public_key.encrypt(int(x))
+            encrypt_timer.toc()
+            encrypted_row += [res]
         encrypted_matrix.append(encrypted_row)
+    print(f"Avg encrypt time = {encrypt_timer.avg()} (x {encrypt_timer.cnt} = {encrypt_timer.total})")
     return encrypted_matrix
 
 
@@ -32,22 +50,41 @@ def paillier_decrypt_matrix(encrypted_matrix, secret_key):
 
 
 def paillier_matrix_multiply(matrix_a, matrix_b, public_key, secret_key):
+    tic = time.time()
     encrypted_matrix_a = paillier_encrypt_matrix(matrix_a, public_key)
+    print(f"encrypt time = {time.time() - tic}")
+
     matrix_b_T = list(zip(*matrix_b))  # Transpose matrix B
     # encrypted_matrix_b_T = paillier_encrypt_matrix(matrix_b_T, public_key)
     encrypted_matrix_b_T = matrix_b_T
 
     result = []
+    prod_timer = Timer()
+    add_timer = Timer()
+    decrypt_timer = Timer()
 
     for i in range(len(matrix_a)):
         result_row = []
         for j in range(len(matrix_b[0])):
-            encrypted_dot_product = encrypted_matrix_a[i][0] * encrypted_matrix_b_T[j][0]
+            prod_timer.tic()
+            res = encrypted_matrix_a[i][0] * encrypted_matrix_b_T[j][0]
+            prod_timer.toc()
+            encrypted_dot_product = res
             for k in range(1, len(matrix_a[0])):
-                encrypted_dot_product += encrypted_matrix_a[i][k] * encrypted_matrix_b_T[j][k]
-            result_row.append(secret_key.decrypt(encrypted_dot_product))
+                prod_timer.tic()
+                res = encrypted_matrix_a[i][k] * encrypted_matrix_b_T[j][k]
+                prod_timer.toc()
+                add_timer.tic()
+                encrypted_dot_product += res
+                add_timer.toc()
+            decrypt_timer.tic()
+            res = secret_key.decrypt(encrypted_dot_product)
+            decrypt_timer.toc()
+            result_row.append(res)
         result.append(result_row)
-
+    print(f"Avg product time = {prod_timer.avg()} (x {prod_timer.cnt} = {prod_timer.total})\n"
+          f"Avg addition time = {add_timer.avg()} (x {add_timer.cnt} = {add_timer.total})\n"
+          f"Avg decryption time = {decrypt_timer.avg()} (x {decrypt_timer.cnt} = {decrypt_timer.total})")
     return result
 
 
@@ -78,18 +115,19 @@ def simulate_secure_aggregation(mat_a, mat_b, num_parties=5):
 if __name__ == "__main__":
 
     public_key, secret_key = paillier.generate_paillier_keypair()
+    print(f"public key n: {public_key.n}")
 
-    for batch_size in [1, 2, 4, 8, 16, 32, 64]:
+    for batch_size in [256]:
         print(f'batch size {batch_size}:')
-        matrix_a = np.random.randint(low=0, high=8, size=(batch_size, 8))
-        matrix_b = np.random.randint(low=0, high=8, size=(8, 8))
+        matrix_a = np.random.randint(low=1, high=1 << 27, size=(256, 2))
+        matrix_b = np.random.randint(low=1, high=1 << 27, size=(2, 2))
         timer.tic()
         result = paillier_matrix_multiply(matrix_a.tolist(), matrix_b.tolist(), public_key, secret_key)
         tr = timer.toc()
         print(f'\tHE cpu_time = {tr}')
         total_time = 0
-        for _ in range(1000):
+        for _ in range(100):
             timer.tic()
             simulate_secure_aggregation(matrix_a, matrix_b)
             total_time += timer.toc()
-        print(f'\tSA cpu_time = {total_time / 1000}\n')
+        print(f'\tSA cpu_time = {total_time / 100}\n')
